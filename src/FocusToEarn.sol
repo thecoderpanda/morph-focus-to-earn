5ca0965f6440c934cbe21b977f13323a8a2b8b85
Shantanu Vishwanadha <mailvishwanadha@gmail.com>
2024-09-21 16:39:46 +0800

    Modified Contracts

--- src/FocusToEarn.sol
+++ src/FocusToEarn.sol
@@ -1,28 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-}
+// Import the SafeERC20 library and IERC20 interface from OpenZeppelin
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract FocusToEarn {
+    using SafeERC20 for IERC20;
+
     IERC20 public immutable usdtToken;
     IERC20 public immutable ftnToken;
 
@@ -38,10 +22,12 @@ contract FocusToEarn {
 
     mapping(address => UserInfo) public userInfo;
 
-    uint256 public constant USDT_DECIMALS = 6;
+    uint256 public constant USDT_DECIMALS = 18;
     uint256 public constant FTN_DECIMALS = 18;
 
     uint256 public constant DEPOSIT_AMOUNT = 200_000; // 0.2 USDT * 1e6
+
+    uint256 public constant REWARD_RATE_PER_SECOND = 10 * (10 ** FTN_DECIMALS); // 10 FTN tokens per second
 
     event Deposit(address indexed user, uint256 amount);
     event TimerStarted(address indexed user, uint256 startTime);
@@ -52,7 +38,7 @@ contract FocusToEarn {
     error DepositRequired();
     error TimerAlreadyStarted();
     error TimerNotStarted();
-    error InsufficientRewards();
+    error NoRewardsToClaim();
 
     constructor(address _usdtToken, address _ftnToken) {
         usdtToken = IERC20(_usdtToken);
@@ -63,8 +49,8 @@ contract FocusToEarn {
         UserInfo storage user = userInfo[msg.sender];
         if (user.hasDeposited) revert AlreadyDeposited();
 
-        bool success = usdtToken.transferFrom(msg.sender, address(this), DEPOSIT_AMOUNT);
-        require(success, "USDT transfer failed");
+        // Use SafeERC20 for token transfer
+        usdtToken.safeTransferFrom(msg.sender, address(this), DEPOSIT_AMOUNT);
 
         user.hasDeposited = true;
 
@@ -88,84 +74,35 @@ contract FocusToEarn {
 
         uint256 focusTime = block.timestamp - user.lastStartTime;
         user.totalFocusTime += focusTime;
-        uint256 rewards = calculateRewards(msg.sender, focusTime);
+        uint256 rewards = calculateRewards(focusTime);
         user.accumulatedRewards += rewards;
         user.isTiming = false;
 
         emit TimerStopped(msg.sender, focusTime, rewards);
     }
 
-    function calculateRewards(address _user, uint256 _focusTime)
+    function calculateRewards(uint256 _focusTime)
         internal
-        view
+        pure
         returns (uint256)
     {
-        uint256 previousFocusTime = userInfo[_user].totalFocusTime;
-        uint256 totalFocusTime = previousFocusTime + _focusTime;
-
-        uint256 rewards = 0;
-        uint256 remainingFocusTime = _focusTime;
-
-        // Tier 1: <1 hour
-        if (previousFocusTime < 1 hours) {
-            uint256 tierLimit = 1 hours - previousFocusTime;
-            uint256 timeInTier = remainingFocusTime < tierLimit
-                ? remainingFocusTime
-                : tierLimit;
-            rewards += timeInTier * (1e16); // 0.01 FTN per second
-            remainingFocusTime -= timeInTier;
-            previousFocusTime += timeInTier;
-        }
-
-        // Tier 2: 1 - 2 hours
-        if (
-            remainingFocusTime > 0 &&
-            previousFocusTime >= 1 hours &&
-            previousFocusTime < 2 hours
-        ) {
-            uint256 tierLimit = 2 hours - previousFocusTime;
-            uint256 timeInTier = remainingFocusTime < tierLimit
-                ? remainingFocusTime
-                : tierLimit;
-            rewards += timeInTier * (2e16); // 0.02 FTN per second
-            remainingFocusTime -= timeInTier;
-            previousFocusTime += timeInTier;
-        }
-
-        // Tier 3: 2 - 5 hours
-        if (
-            remainingFocusTime > 0 &&
-            previousFocusTime >= 2 hours &&
-            previousFocusTime < 5 hours
-        ) {
-            uint256 tierLimit = 5 hours - previousFocusTime;
-            uint256 timeInTier = remainingFocusTime < tierLimit
-                ? remainingFocusTime
-                : tierLimit;
-            rewards += timeInTier * (3e16); // 0.03 FTN per second
-            remainingFocusTime -= timeInTier;
-            previousFocusTime += timeInTier;
-        }
-
-        // Tier 4: >5 hours
-        if (remainingFocusTime > 0 && previousFocusTime >= 5 hours) {
-            uint256 timeInTier = remainingFocusTime;
-            rewards += timeInTier * (5e16); // 0.05 FTN per second
-        }
-
+        uint256 rewards = _focusTime * REWARD_RATE_PER_SECOND;
         return rewards;
     }
 
     function claimRewards() external {
         UserInfo storage user = userInfo[msg.sender];
-        if (user.accumulatedRewards < 1e18) revert InsufficientRewards();
-
         uint256 rewardsToClaim = user.accumulatedRewards;
+
+        if (rewardsToClaim == 0) revert NoRewardsToClaim();
+
+        uint256 contractBalance = ftnToken.balanceOf(address(this));
+        if (contractBalance < rewardsToClaim) revert("Insufficient contract FTN balance");
+
         user.accumulatedRewards = 0;
         totalRewardsClaimed += rewardsToClaim;
 
-        bool success = ftnToken.transfer(msg.sender, rewardsToClaim);
-        require(success, "FTN transfer failed");
+        ftnToken.safeTransfer(msg.sender, rewardsToClaim);
 
         emit RewardsClaimed(msg.sender, rewardsToClaim);
     }
@@ -185,5 +122,10 @@ contract FocusToEarn {
         totalFocusTime = user.totalFocusTime;
         accumulatedRewards = user.accumulatedRewards;
         isTiming = user.isTiming;
+    }
+
+    // function to check the contract's FTN token balance
+    function getContractFTNBalance() external view returns (uint256) {
+        return ftnToken.balanceOf(address(this));
     }
 }
--- src/FocusToken.sol
+++ src/FocusToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FocusToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("FocusToken", "FTN") {
+contract FocusToEarnToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("FocusToken", "FTNS") {
         _mint(msg.sender, initialSupply);
     }
 }
